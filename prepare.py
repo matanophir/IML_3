da241a51aff10c9264880c071f7aa376a9d67bf4
Matan Ophir <matanophir1@gmail.com>
2025-01-20 00:00:41 +0200

    changed prepare.py to use all the data

--- prepare.py
+++ prepare.py
@@ -2,16 +2,37 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
 
-def prepare_data(training_data, new_data):
-    trans_df = new_data.copy()
+def preprocess_data(data):
+    trans_df = data.copy()
+
+    #extracting features:
+    # trans_df['pcr_date'] = pd.to_datetime(trans_df['pcr_date'])
+    # trans_df['month'] = trans_df['pcr_date'].dt.month
+    # trans_df['year'] = trans_df['pcr_date'].dt.year
+
+    trans_df['blood_type_group'] = trans_df["blood_type"].isin(["O+", "B+"])
+
+    #dropping columns:
+    trans_df.drop(columns = ["blood_type",'current_location', 'patient_id','pcr_date'], inplace = True)
+
+    #converting catarogical columns:
+    trans_df['sex'] = trans_df['sex'].map({'M': 1, 'F': -1})
+    trans_df['blood_type_group'] = trans_df['blood_type_group'].map({True: 1, False: -1})
 
     #filling missing values:
-    trans_df.fillna(training_data[['household_income', 'PCR_02']].median(), inplace = True)
-    trans_df.fillna(training_data[['household_income', 'PCR_02']].median(), inplace = True)
+    trans_df.fillna(trans_df[['household_income', 'PCR_02']].median(), inplace = True)
+
+    return trans_df
+
+def prepare_data(training_data, new_data):
+    trans_df = new_data
 
     #normalizing data:
-    pcr_cols = set(training_data.columns[training_data.columns.str.startswith("PCR")])
-    minmax_cols = set(["PCR_04", "PCR_06", "PCR_03"])
+    # pcr_cols = set(training_data.columns[training_data.columns.str.startswith("PCR")])
+    minmax_cols = set(["PCR_04", "PCR_06", "PCR_03",'conversations_per_day','household_income'])
+    
+    exclude_cols = {'sex','blood_type_group', 'contamination_level'}
+    zscore_cols = set(training_data.columns) - minmax_cols - exclude_cols
 
     minmax_scaler = MinMaxScaler((-1,1))
     minmax_mask = training_data.columns.isin(minmax_cols)
@@ -19,21 +40,20 @@ def prepare_data(training_data, new_data):
     trans_df.loc[:, minmax_mask] = minmax_scaler.transform(trans_df.loc[:, minmax_mask])
 
     zscore_scaler = StandardScaler()
-    zsocre_mask = training_data.columns.isin(pcr_cols.difference(minmax_cols))
+    zsocre_mask = training_data.columns.isin(zscore_cols)
     zscore_scaler.fit(training_data.loc[:, zsocre_mask])
     trans_df.loc[:, zsocre_mask] = zscore_scaler.transform(trans_df.loc[:, zsocre_mask])
 
-    #adding and dropping features:
-    trans_df['blood_type_group'] = trans_df["blood_type"].isin(["O+", "B+"])
-    trans_df.drop(columns = ["blood_type"], inplace = True)
 
     return trans_df
 
 
 if __name__ == "__main__":
-    virus_data = pd.read_csv('virus_data.csv')
+    virus_data = pd.read_csv('data_HW3.csv')
 
     train_df, test_df = train_test_split(virus_data, test_size = 0.2, random_state = 134)
+
+    train_df, test_df = preprocess_data(train_df), preprocess_data(test_df)
 
     # Prepare training set according to itself
     train_df_prepared = prepare_data(train_df, train_df)
